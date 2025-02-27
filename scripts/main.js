bf6ce955bff554c54058685188016ea2d27baf09
newyorkmango <89069595+newyorkmango@users.noreply.github.com>
2025-02-27 21:53:24 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `Happy Birthday, ${myName}`;
   }
 }
 
@@ -26,7 +26,7 @@ if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  myHeading.textContent = `Happy Birthday, ${storedName}`;
 }
 
 myButton.addEventListener("click", () => {
